94b496ee47cc87acb6f4f9614022f2ff6c8d7cf5
Christopher Johns <tyrantwave@gmail.com>
2019-02-12 15:39:12 +0900

    Saves lists to local storage and displays at the bottom. Can't load yet.

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Blazor.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
+using Newtonsoft.Json;
+using Blazor.Extensions.Storage;
+
 namespace TeacherTools
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddStorage();
         }
 
         public void Configure(IBlazorApplicationBuilder app)
