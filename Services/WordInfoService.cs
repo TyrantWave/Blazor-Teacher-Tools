350a6bbd0f0a52523bca535d386c43bbfe2aa655
Christopher Johns <tyrantwave@gmail.com>
2019-03-06 15:56:01 +0900

    Word of the day, gets a new random word each day.
    Stores the last obtained word & date in localstorage.

--- Services/WordInfoService.cs
+++ Services/WordInfoService.cs
@@ -3,25 +3,36 @@ using Microsoft.AspNetCore.Blazor;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 using TeacherTools.Credentials;
-
 namespace TeacherTools.Services
 {
 	public class WordInfoService
 	{
 		private readonly HttpClient http;
+		private readonly Uri baseUrl = new Uri("https://wordsapiv1.p.mashape.com/words/");
 
 		public WordInfoService(HttpClient client)
 		{
 			http = client;
+			http.DefaultRequestHeaders.Accept.Clear();
+			http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			http.DefaultRequestHeaders.Add("X-Mashape-Key", Credentials.Credentials.WordsApiKey);
 		}
-		public async Task<WordInfo> GetWordAsync()
+
+		public async Task<WordInfo> GetWordAsync(string word)
 		{
-			return await http.GetJsonAsync<WordInfo>("sample-data/words-api.json");
-
+			var request = new Uri(baseUrl, word);
+			return await http.GetJsonAsync<WordInfo>(request.ToString());
 		}
 
+		public async Task<WordInfo> GetRandomWordAsync()
+		{
+			var request = new UriBuilder(baseUrl);
+			request.Query = "hasDetails=examples,definition&random=true";
+			return await http.GetJsonAsync<WordInfo>(request.ToString());
+		}
 	}
 }
