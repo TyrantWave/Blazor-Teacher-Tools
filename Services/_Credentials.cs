f640bfa8e2bba04bc33102d40387d8e1da2cd0e0
Christopher Johns <tyrantwave@gmail.com>
2019-09-08 15:02:13 +0100

    Updated example

--- Services/_Credentials.cs
+++ Services/_Credentials.cs
@@ -1,5 +1,7 @@
-namespace TeacherTools.Credentials {
-    class Credentials {
-        public static string WordsApiKey = "YOUR_WORDSAPI_KEY";
+/** Example credentials file. Copy into `Credentials.cs` and change the api key.
+    namespace TeacherTools.Credentials {
+        class Credentials {
+            public static string WordsApiKey = "YOUR_WORDSAPI_KEY";
+        }
     }
-}
+ */
